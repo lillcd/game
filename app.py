e330abc5f7f4c9e0586067a5ab8e4cf54770b66c
lillcd <chris.lilly72@gmail.com>
2025-09-15 07:42:29 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ if "used_rounds" not in st.session_state:
 def render_round():
     global current_round
     unused_rounds = [r for r in rounds.keys() if r not in st.session_state.used_rounds]
-	if not unused_rounds:
+    if not unused_rounds:
         # All rounds used — reset as all played
         st.session_state.used_rounds = []
         unused_rounds = list(rounds.keys())
@@ -553,7 +553,7 @@ if st.session_state.round_html:
     components.html(st.session_state.round_html, height=582, scrolling=False)
                 
 if st.session_state.show_text:
-    st.title("💡 Welcome to KeyWordGame!")
+    st.title("👋 Welcome to KeyWordGame!")
     st.write("Make your way across the board from left to right by placing the blocks in a line, connecting them like dominoes using hidden related links. In this example, the links are 'ORANGE' (Seville orange, Orange County) and 'RED' (Simply red, red herring).")
     components.html(demo_html_code, height=60, scrolling=False)
     st.markdown('<p style="font-size: 10px; color: gray; margin-top: 30px;">Copyright (c) 2025 Chris Lilly</p>', unsafe_allow_html=True)
